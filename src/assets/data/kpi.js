3be5340e6fd1d0f91157c68e4e1bc59801d9c9c5
DESKTOP-UNSUA5H\tritr <tritmse160278@fpt.edu.vn>
2024-07-09 18:35:46 +0700

    mark report for intern

---
+++ src/assets/data/kpi.js
@@ -0,0 +1,120 @@
+export const kpi = [
+    {
+        TrainingProgramID: "1",
+        TrainingProgramName: "React Basic",
+        startDate: "25/06/2021",
+        endDate: "25/07/2021",
+        internID: "1",
+        GradeCatagory: [
+            {
+                CatagoryName: "Participation",
+                gradeItem: [
+                    {
+                        gradeItem: "Participation",
+                        weight: "10%",
+                        value: "10",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Project",
+                gradeItem: [
+                    {
+                        gradeItem: "Project",
+                        weight: "30%",
+                        value: "10",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Quiz",
+                gradeItem: [
+                    {
+                        gradeItem: "Quiz 1",
+                        weight: "15%",
+                        value: "10",
+                    },
+                    {
+                        gradeItem: "Quiz 2",
+                        weight: "15%",
+                        value: "10",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Presentation",
+                gradeItem: [
+                    {
+                        gradeItem: "Presentation 1",
+                        weight: "15%",
+                        value: "9",
+                    },
+                    {
+                        gradeItem: "Presentation 2",
+                        weight: "15%",
+                        value: "8",
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        TrainingProgramID: "2",
+        TrainingProgramName: ".Net Basic",
+        startDate: "25/06/2021",
+        endDate: "25/07/2021",
+        internID: "1",
+        GradeCatagory: [
+            {
+                CatagoryName: "Participation",
+                gradeItem: [
+                    {
+                        gradeItem: "Participation",
+                        weight: "10%",
+                        value: "5",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Project",
+                gradeItem: [
+                    {
+                        gradeItem: "Project",
+                        weight: "30%",
+                        value: "4",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Quiz",
+                gradeItem: [
+                    {
+                        gradeItem: "Quiz 1",
+                        weight: "15%",
+                        value: "7",
+                    },
+                    {
+                        gradeItem: "Quiz 2",
+                        weight: "15%",
+                        value: "7",
+                    }
+                ]
+            },
+            {
+                CatagoryName: "Presentation",
+                gradeItem: [
+                    {
+                        gradeItem: "Presentation 1",
+                        weight: "15%",
+                        value: "4",
+                    },
+                    {
+                        gradeItem: "Presentation 2",
+                        weight: "15%",
+                        value: "3",
+                    }
+                ]
+            }
+        ]
+    }
+]
